87188434cd5cce4813b6de64d148ea8f7c1e303e
M. Faturachman A <atthaariq@student.untan.ac.id>
2022-05-26 10:54:42 +0700

    add some tile map

--- app_gis/resources/views/home.blade.php
+++ app_gis/resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UAS WEB GIS</title>
 
+    <!-- Leaflet CSS-->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
    integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
    crossorigin=""/>
@@ -15,8 +16,13 @@
    integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
    crossorigin=""></script>
 
+    <!-- Load Esri Leaflet from CDN -->
+    <script src="https://unpkg.com/esri-leaflet@3.0.8/dist/esri-leaflet.js"
+    integrity="sha512-E0DKVahIg0p1UHR2Kf9NX7x7TUewJb30mxkxEm2qOYTVJObgsAGpEol9F6iK6oefCbkJiA4/i6fnTHzM6H1kEA=="
+    crossorigin=""></script>
+
     <style>
-       #map { height: 500px; }
+       #map { height: 600px; }
     </style>
 </head>
 <body>
@@ -27,6 +33,15 @@ UAS WEB GIS
 <script>
     var map = L.map('map').setView([-0.0244467,109.339112], 13);
     
+    // google 
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+        maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(map);
+
+    // esri
+    // L.esri.basemapLayer('Topographic').addTo(map);
+
+    /* Basemap MapBox (openstreet)
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -35,7 +50,7 @@ UAS WEB GIS
 		tileSize: 512,
 		zoomOffset: -1
 	}).addTo(map);
-    
+    */
 </script>
 
 </html>
